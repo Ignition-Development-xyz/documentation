e6507e41a0cf3bb54abdcd631aa9b012e7844782
MythicalDoggo <87282334+MythicalDoggo@users.noreply.github.com>
2022-11-25 19:43:56 +0100

    LETS GO

---
+++ docs/introduction.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+Welcome to the 
+Ignition-Development Documentation, this is where you will find documentation for all our projects. You can contribute to our documentation through the [Github repo](https://github.com/Ignition-Development/documentation).
+
+# Current Projects
+1. IgnitionDash (Soon)
+2. IgnitionLauncher (Soon)
---
+++ docusaurus.config.js
@@ -0,0 +1,79 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ignition-Development',
+  tagline: 'A new and reliable development team that looks towards improving security of new web apps and more.',
+  url: 'https://ignition-development.xyz',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'https://avatars.githubusercontent.com/u/119068402',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Ignition-Development', // Usually your GitHub org/user name.
+  projectName: 'Ignition-Development', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ignition-Development',
+        logo: {
+          alt: 'IGH Logo',
+          src: 'https://avatars.githubusercontent.com/u/119068402',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+
+        copyright: `Copyright © ${new Date().getFullYear()} Ignition-Development. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
