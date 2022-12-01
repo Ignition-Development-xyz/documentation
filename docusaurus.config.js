2c3144b7df445e32a48c8159e686ca3540e90c7c
MythicalDoggo <87282334+MythicalDoggo@users.noreply.github.com>
2022-12-01 21:40:00 +0100

    Ups

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Ignition-Development/documentation',
+            'https://github.com/Ignition-Development/documentation/blob/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
